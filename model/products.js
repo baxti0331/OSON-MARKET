30a4f12183eefbf2fce2c7c445dfdc3a86416983
baxti0331 <sheraliyevbaxtiyor3103@gmail.com>
2025-06-08 03:53:49 +0300

    Create products.js

---
+++ model/products.js
@@ -0,0 +1,23 @@
+export const products = [
+  {
+    id: 1,
+    name: "Смартфон XYZ",
+    price: 19999,
+    image: "/assets/phone1.jpg",
+    description: "Мощный смартфон с большим экраном",
+  },
+  {
+    id: 2,
+    name: "Наушники ABC",
+    price: 4999,
+    image: "/assets/headphones1.jpg",
+    description: "Беспроводные наушники с хорошим звуком",
+  },
+  {
+    id: 3,
+    name: "Ноутбук QWE",
+    price: 55999,
+    image: "/assets/laptop1.jpg",
+    description: "Легкий и быстрый ноутбук для работы и игр",
+  },
+];
